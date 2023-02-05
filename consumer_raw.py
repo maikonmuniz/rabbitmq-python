c47e1e0a82994c0342418c5f2b8f3c0bde1f9e6b
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-02-05 20:11:35 -0300

    project complete rabbitmq on python

---
+++ consumer_raw.py
@@ -0,0 +1,32 @@
+import pika
+
+
+def my_callback(ch, method, properties, body):
+    print(body)
+
+
+connection_parameters = pika.ConnectionParameters(
+    host="localhost",
+    port=5672,
+    credentials=pika.PlainCredentials(
+        username="guest",
+        password="guest"
+    )
+)
+
+channel = pika.BlockingConnection(connection_parameters).channel()
+
+channel.queue_declare(
+    queue="",
+    durable=True
+)
+
+channel.basic_consume(
+    queue="my_queue",
+    auto_ack=True,
+    on_message_callback=my_callback
+)
+
+print(f'Listen RabbitMQ on Port 5672')
+
+channel.start_consuming()
